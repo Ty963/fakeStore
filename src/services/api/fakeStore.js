ca5206446a29dff981cec3f330c63742bbf1580f
Ty963 <you@example.com>
2025-11-15 14:40:19 +0100

    added some functions for API interaction, almost done with the API

--- src/services/api/fakeStore.js
+++ src/services/api/fakeStore.js
@@ -18,7 +18,32 @@ const fakeStoreApi = {
        }
     },
 
-    async getSingleProduct(productId){
+    // post request axios.post(url, data, config)
+    async addNewProduct(id, title, price, description, category, image) {
+        const url = FAKESTORE_API_URL + "/products";
+        try {
+            const response = await axios.post(url,
+                {
+                    "id": id,
+                    "title": title,
+                    "price": price,
+                    "description": description,
+                    "category": category,
+                    "image": image
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            throw error
+        }
+    },
+
+    async getSingleProduct(productId) {
         const url = FAKESTORE_API_URL + "/products/" + productId;
         try {
             const response = await axios.get(url);
@@ -26,6 +51,43 @@ const fakeStoreApi = {
         } catch (error) {
             const errorInfo = handleGetErrors(error, url);
             console.error(errorInfo.message);
+        }
+    },
+
+
+// TODO Check this updateProduct function
+// If I can make a feeder instead of feeding everything as props then I'd be able to send only then changed fields and the rest would remain the same, API documentation says it's possible and ought to be done with that way, double check the veracity of this and otherwise nest inside an if where I check if the updated field is different from the original one and define a constant that can be fed as a prop while maintaining original values for unchanged fields
+    async updateProduct(id, updatedTitle, updatedPrice, updatedDescription, updatedCategory, updatedImage) {
+        const url = FAKESTORE_API_URL + "/products/" + productId;
+        try {
+            const response = await axios.put(url, {
+                "id": id,
+                "title": updatedTitle,
+                "price": updatedPrice,
+                "description": updatedDescription,
+                "category": updatedCategory,
+                "image": updatedImage
+                }, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            throw error
+        }
+    },
+
+    // TODO Check this deleteProduct function
+    async deleteProduct(productId) {
+        const url = FAKESTORE_API_URL + "/products/" + productId;
+        try {
+            const response = await axios.delete(url);
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            throw error
         }
     },
 
