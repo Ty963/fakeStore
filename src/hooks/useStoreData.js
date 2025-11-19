762117d77f2b1729b1f30049e4859b1c4393670d
Ty963 <you@example.com>
2025-11-19 22:44:57 +0100

    Polished up the hooks, gotta add some thought process text to the the readme next

---
+++ src/hooks/useStoreData.js
@@ -0,0 +1,98 @@
+import { useState, useCallback } from 'react';
+import { fakeStoreApi } from '../services/api/fakeStoreApi.js';
+
+export function useStoreData() {
+    const [userData, setUserData] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setLoading] = useState(false);
+
+    const fetchAllProducts = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getAllProducts();
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching products');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const fetchSingleProduct = useCallback(async (productId) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getSingleProduct(productId);
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching product');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const fetchAllCarts = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getAllCarts();
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching carts');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const fetchSingleCart = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getSingleCart();
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching carts');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const fetchAllUsers = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getAllUsers();
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching carts');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const fetchSingleUser = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const data = await fakeStoreApi.getSingleUser();
+            return data;
+        } catch (err) {
+            setError(err.message || 'Error fetching carts');
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+    
+    return {
+        error,
+        isLoading,
+        fetchAllProducts,
+        fetchSingleProduct,
+        fetchAllCarts,
+        fetchSingleCart,
+        fetchAllUsers,
+        fetchSingleUser
+    };
+}
+
