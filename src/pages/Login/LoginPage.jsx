d27035c902404b5d023738194ad6cee92d8a4af7
Ty963 <you@example.com>
2025-11-22 14:38:47 +0100

    added all functionalities to the login page, login page now fully functional, next up adding user context and local storage

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,15 +1,13 @@
 import { useForm } from "react-hook-form"
+import fakeStoreApi from "../../services/api/fakeStoreApi";
 
 export default function LoginPage() {
     // const { register, handleSubmit, formState: { errors } } = useForm();
     const { register, handleSubmit } = useForm();
 
-    function handleSubmit() {
-        // Handle login logic here
-    }
-
-    function handleFormSubmit(data) {
-        console.log(data);
+    async function handleFormSubmit(data) {
+        const response = await fakeStoreApi.authenticateUser(data.username, data.password);
+        console.log(response);
     }
 
     return (
@@ -21,13 +19,16 @@ export default function LoginPage() {
             </h2>
 
             <div className="input-group">
-                <label htmlFor="email">Email:</label>
+                <label htmlFor="username">Username:</label>
                 <input 
-                    type="email" 
-                    id="email" 
-                    name="email" 
-                    {...register('email')}
-                    required
+                    type="text"
+                    id="username" 
+                    name="username" 
+                    {...register('username', {
+                        required: {
+                        value: true,
+                        message: 'This field is required',
+                    }})}
                 />
             </div>
 
@@ -37,14 +38,18 @@ export default function LoginPage() {
                     type="password" 
                     id="password"
                     name="password"
-                    {...register('password')}
-                    required
+                    {...register('password', {
+                        required: {
+                        value: true,
+                        message: 'This field is required',
+                    }})}
                 />
             </div>
 
             <button type="submit" className="btn">
                 Login
             </button>
+
         </form>
     </div>
     )
